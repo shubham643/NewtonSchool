58791047154d89dbffbb79708d5eb2f00fefe5ae
Shubham Bansal <shubhambansal643@gmail.com>
2022-03-12 00:57:50 +0530

    Create n-aryTree.java

---
+++ n-aryTree.java
@@ -0,0 +1,14 @@
+// any number of children --> n-ary tree. n maximum number of children
+
+class Node {
+    int value;
+    Node[] child = new Node[10];
+}
+
+
+
+
+
+public class GenericTree {
+    Node root;
+}
